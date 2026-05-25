1b351d9e014e00a17095d55274756e3eade297ae
Tanish Mishra <74447173+Tan28-art@users.noreply.github.com>
2026-05-25 11:36:51 -0700

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        count_left = 1
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                # you can delete at either left or right
+                # delete left
+                left += 1
+                count_left -= 1
+                
+        left = 0
+        right = len(s) - 1
+        count_right = 1
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                # you can delete at either left or right
+                # delete left
+                right -= 1
+                count_right -= 1
+
+        if count_left >= 0 or count_right >= 0:
+            return True
+        return False
