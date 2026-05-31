76a5f1ec005778e88b695a0c73514cc97cc09d6b
Tanish Mishra <74447173+Tan28-art@users.noreply.github.com>
2026-05-31 16:38:41 -0700

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+        for num in nums:
+            if hashmap.get(num):
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+
+        print(hashmap)
+        max_val = hashmap[nums[0]]
+        result = nums[0]
+
+        for k, v in hashmap.items():
+            if v > max_val:
+                max_val = v
+                result = k
+
+        return result
