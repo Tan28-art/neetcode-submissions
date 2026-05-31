554869eb40aa6b7e1b6bcee8f779a6c1debf7c7b
Tanish Mishra <74447173+Tan28-art@users.noreply.github.com>
2026-05-31 16:59:29 -0700

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # More space efficient approach uses boyer-moore algorthm
+        # essentially keep count variable, increase for res, dec otherwise
+        # if 0 and next element is diff than res, change res and count += 1
+        # majority element will outvote the rest of the array
+
+        result = nums[0]
+        count = 0
+        for num in nums:
+            if num == result:
+                count += 1
+
+            else:
+                count -= 1
+            
+            if count == 0:
+                result = num
+                count += 1
+
+        return result
