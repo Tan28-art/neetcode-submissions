4bc32d95afddfb7b2addd8cb513f72e2cb829f05
Tanish Mishra <74447173+Tan28-art@users.noreply.github.com>
2026-06-06 18:03:17 -0700

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = 0
+        n = len(nums)
+        right = n -1
+
+        while left <= right:
+            if nums[left]==val:
+                # swap the element to the end of the list
+                nums[left] =  nums[right]
+
+                # move right pointer behind
+                right -= 1
+
+                # dont move left cause we still have to check left element
+            else:
+                left += 1
+
+        return right + 1
